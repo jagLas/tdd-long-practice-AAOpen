de56ecda7f0beb0c2e744754dfbf6f04ba190dbe
jagLas <jglasher@gmail.com>
2022-12-16 14:06:10 -0500

    triangle spec finished

--- problems/triangle.js
+++ problems/triangle.js
@@ -1,1 +1,21 @@
-// Your code here
+class Triangle {
+    constructor(a, b, c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
+
+class Scalene {
+
+}
+
+class Isosceles {
+
+}
+
+module.exports = {
+    Triangle,
+    Scalene,
+    Isosceles
+}
--- test/triangle-spec.js
+++ test/triangle-spec.js
@@ -1,1 +1,47 @@
-// Your code here
+const {expect} = require('chai');
+const {Triangle, Scalene, Isosceles} = require('../problems/triangle')
+
+let a, b, c;
+let a2, b2, c2;
+let a3, b3, c3;
+let triangle, triangle2, triangle3, invalidTri, invalidTri2;
+
+describe('Triangle Class', function () {
+    beforeEach( ()=> {
+        a = 2, b = 3, c = 4;
+        a2 = 1, b2 = 2, c2 = 5;
+        a3 = 0, b3 = 3, c3 = 3;
+        triangle = new Triangle(2, 3, 4);
+        invalidTri = new Triangle(5, 1, 2);
+        invalidTri2 = new Triangle (0, 3, 3);
+
+    })
+
+    it('should set properites for all 3 sides', function () {
+        expect(triangle.a).to.equal(2);
+        expect(triangle.b).to.equal(3);
+        expect(triangle.c).to.equal(4);
+    })
+
+    it('getPerimeter() should return the perimeter of the triangle', () => {
+        let perimeter = triangle.getPerimeter();
+        expect(perimeter).to.equal(9);
+    });
+
+    it('hasValidSideLengths() returns true if triangle is valid, and false if not', () => {
+        expect(triangle.hasValidSideLengths()).to.be.true;
+        expect(invalidTri.hasValidSideLengths()).to.be.false;
+        expect(invalidTri2.hasValidSideLengths()).to.be.false;
+    });
+
+    it('validate() should add an isValid property and set to true if a valid triangle', () => {
+        expect(triangle.isValid).to.be.undefined;
+        triangle.validate();
+        expect(triangle.isValid).to.be.true;
+    });
+
+    it('getValidTriangles() take in an arbitrary number of triangles and returns all that are valid', () => {
+        let res = getValidTriangles(triangle, invalidTri, invalidTri2);
+        expect(res).to.deep.equal([triangle]);
+    })
+})
