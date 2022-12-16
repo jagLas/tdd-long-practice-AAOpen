5d90b79f4e6494571dc10b8fc7fbae35f5a4dc79
jagLas <jglasher@gmail.com>
2022-12-15 21:30:42 -0500

    end phase 1

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,6 +1,11 @@
 module.exports = function reverseString(string) {
-  let reverseString = string.split('');
-  reverseString.reverse();
+  if (typeof string !== 'string') {
+    throw new TypeError('reverseString requires a string as an input');
+  }
+
+  //sets the string as an array and reverses it
+  let reverseString = string.split('').reverse();
+  //rejoins the array into a string
   reverseString = reverseString.join('');
 
   return reverseString;
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -7,4 +7,14 @@ describe('Reverse String Function', function(){
         const string = 'fun';
         expect(reverseString(string)).to.equal('nuf')
     })
+
+    it('should throw a TypeError when input is not a string', function(){
+        const num = 52;
+        const array = ['s', 'd', 'f'];
+        const object = {a: 2, b: 3}
+
+        expect(() => reverseString(num)).to.throw(TypeError);
+        expect(() => reverseString(array)).to.throw(TypeError);
+        expect(() => reverseString(object)).to.throw(TypeError);
+    })
 })
