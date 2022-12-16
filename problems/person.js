b6ba5350e37dfffe96657fad27ebceabf1d587f9
jagLas <jglasher@gmail.com>
2022-12-16 13:33:10 -0500

    finished phase 4

--- problems/person.js
+++ problems/person.js
@@ -23,8 +23,31 @@ class Person {
     return greetings;
   }
 
-  
+  update(obj) {
+    if (typeof obj !== 'object'){
+      throw TypeError('requires an object');
+    } else if(!obj.name || !obj.age) {
+      throw TypeError('object needs a name and age');
+    }
+
+    this.name = obj.name;
+    this.age = obj.age;
+  }
+
+  tryUpdate(obj){
+    try{
+      this.update(obj);
+    } catch {
+      return false;
+    }
+    return true;
+  }
 
 }
+
+// let name = 'Billy';
+// let age = 23;
+// let person = new Person (name, age);
+// // person.update('string')
 
 module.exports = Person;
