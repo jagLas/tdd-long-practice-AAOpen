52019dc2873cc64782e7c989474eb08ab77454f4
jagLas <jglasher@gmail.com>
2022-12-16 09:51:31 -0500

    inital spec and code

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,9 +1,19 @@
 function returnsThree() {
-  // Your code here
+  return 3;
 }
 
 function reciprocal(n) {
-  // Your code here
+
+  //checks for edge cases like inputs of 0 and undefined
+
+  if(n === 0) {
+    return undefined;
+  } else if(n === undefined) {
+    return 0;
+  }
+
+  //returns the reciprocal
+  return 1/n;
 }
 
 module.exports = {
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,26 @@
-// Your code here
+const {expect} = require ('chai');
+const {returnsThree, reciprocal} = require('../problems/number-fun')
+
+describe('returnsThree function', function(){
+    it('should return a 3', function() {
+
+        expect(returnsThree()).to.equal(3);
+    })
+})
+
+describe('reciprocal(num)', function(){
+    it('should take a number as an argument and return the reciprocal', function() {
+        expect(reciprocal(3)).to.equal(1/3);
+        expect(reciprocal(1)).to.equal(1);
+        expect(reciprocal(1/5)).to.equal(5);
+        expect(reciprocal(3/4)).to.equal(4/3);
+    })
+
+    it('should take 0 and return undefined', function() {
+        expect(reciprocal(0)).to.equal(undefined);
+    }) 
+
+    it('should take undefined and return 0', function() {
+        expect(reciprocal(undefined)).to.equal(0);
+    })
+})
