e1ca7049a2c501bdc0dae27a713728019cb051df
jagLas <jglasher@gmail.com>
2022-12-16 10:20:31 -0500

    refactor

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,11 +1,15 @@
 function myMap(inputArray, callback) {
+  //checks if callback is a function and returns original array if not
   if (typeof callback !== 'function') {
     return inputArray;
   }
+
+  //applies the call to each element and pushes to new array
   const mappedArray = [];
   inputArray.forEach(el => mappedArray.push(callback(el)));
 
   return mappedArray;
+
 }
 
 module.exports = myMap;
